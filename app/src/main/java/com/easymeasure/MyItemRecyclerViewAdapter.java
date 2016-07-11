7a501f97fbfe64ba46088fef58c1412ed64204bd
sihlemabaleka <mabalekasihle@gmail.com>
2016-07-11 16:28:54 +0200

    Version 1.2

--- app/src/main/java/com/easymeasure/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/com/easymeasure/MyItemRecyclerViewAdapter.java
@@ -4,11 +4,19 @@ import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateUtils;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import com.easymeasure.model.BaseClothingMeasurements;
 import com.easymeasure.model.Client;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
 import java.util.Date;
 import java.util.List;
@@ -54,9 +62,34 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
                 System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
     }
 
+    public void showPopup(final Client client, View v) {
+        PopupMenu popup = new PopupMenu(mContext, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                ParseQuery<BaseClothingMeasurements> query = ParseQuery.getQuery(BaseClothingMeasurements.class);
+                query.getInBackground(client.getObjectId(), new GetCallback<BaseClothingMeasurements>() {
+                    public void done(BaseClothingMeasurements object, ParseException e) {
+                        if (e == null) {
+                            object.getClient().deleteInBackground();
+                            object.deleteInBackground();
+                        } else {
+                            // something went wrong
+                        }
+                    }
+                });
+                return true;
+            }
+        });
+        inflater.inflate(R.menu.item_menu, popup.getMenu());
+        popup.show();
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
         public final TextView mIdView, mContentView, mSizeView;
+        public final ImageButton btnOptions;
         public Client mItem;
 
         public ViewHolder(View view) {
@@ -65,10 +98,17 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
             mIdView = (TextView) view.findViewById(R.id.client_name);
             mContentView = (TextView) view.findViewById(R.id.gender);
             mSizeView = (TextView) view.findViewById(R.id.size);
+            btnOptions = (ImageButton) view.findViewById(R.id.options);
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mContext.getFragmentManager().beginTransaction().replace(R.id.container, ClientMeasurementDetails.newInstance(mItem.getObjectId())).addToBackStack("").commit();
+                }
+            });
+            btnOptions.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showPopup(mItem, v);
                 }
             });
         }
@@ -79,4 +119,5 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
         }
 
     }
+
 }
